ff579e5ff8e23bc64f6fec7bcb66c0498d570023
Robson Rosa <Robson Rosa@DESKTOP-D841IUH>
2023-05-25 10:40:57 -0300

    Commit melhoramento do EnumDificulade e criação da main com inclusão de palavra

--- src/Modelo/EnumDificuldade.java
+++ src/Modelo/EnumDificuldade.java
@@ -13,4 +13,6 @@ public enum EnumDificuldade {
             default : return "";
         }
     }
+    
+
 }
--- src/VisaoConsole/JogoDaForca.java
+++ src/VisaoConsole/JogoDaForca.java
@@ -9,8 +9,23 @@ public class JogoDaForca {
         
         //Como pegar do usuario com Console 
         String nome = Console.input("Digite o Nome");
-        String dif = Console.input("Digite a dificuldade:");
-        Palavra p = new Palavra(EnumDificuldade.dif, nome);
+        String dificuldade = Console.input("1 - " + EnumDificuldade.F.getDescricao() +
+                                           "\n2 - " + EnumDificuldade.M.getDescricao() + 
+                                           "\n3 - " + EnumDificuldade.D.getDescricao() + 
+                                           "\nDigite a dificuldade:");
+        if("1".equals(dificuldade)){
+            Palavra p = new Palavra(EnumDificuldade.F, nome); 
+        }
+        else if("2".equals(dificuldade)){
+            Palavra p = new Palavra(EnumDificuldade.M, nome);
+        }
+        else if("3".equals(dificuldade)){
+            Palavra p = new Palavra(EnumDificuldade.D, nome);
+        }
+        else{
+            Console.output("Dificulade incorreta, digite novamente:", 1);
+        }
+        
         
 
     }
